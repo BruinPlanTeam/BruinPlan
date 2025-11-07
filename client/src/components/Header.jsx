98de8800624bb1d34b433300792704afb0b1ef32
Owen_Battles <owen.battles@icloud.com>
2025-11-06 16:59:35 -0800

    revamped the home page, only background and header are in place

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,49 @@
-import React from 'react'
-import BruinplanLogo from './BruinplanLogo';
-import { MajorCatalog } from './MajorCatalog';
-import '../App.css' 
+import CardNav from './CardNav';
+import logo from '../assets/logo.svg';
 
-export function Header() {
-    return (
-        <div className='header-container'>
-            <BruinplanLogo className='header-logo'/>
-            <div className='right-header-container'>
-                <MajorCatalog />
-            </div>   
-        </div>
-    );
-}
+export const Header = () => {
+  const items = [
+    {
+      label: "About",
+      bgColor: "#0D0716",
+      textColor: "#fff",
+      links: [
+        { label: "Company", ariaLabel: "About Company" },
+        { label: "Careers", ariaLabel: "About Careers" }
+      ]
+    },
+    {
+      label: "Projects", 
+      bgColor: "#170D27",
+      textColor: "#fff",
+      links: [
+        { label: "Featured", ariaLabel: "Featured Projects" },
+        { label: "Case Studies", ariaLabel: "Project Case Studies" }
+      ]
+    },
+    {
+      label: "Contact",
+      bgColor: "#271E37", 
+      textColor: "#fff",
+      links: [
+        { label: "Email", ariaLabel: "Email us" },
+        { label: "Twitter", ariaLabel: "Twitter" },
+        { label: "LinkedIn", ariaLabel: "LinkedIn" }
+      ]
+    }
+  ];
+
+  return (
+    <CardNav
+      logo={logo}
+      logoAlt="Company Logo"
+      items={items}
+      baseColor="#fff"
+      menuColor="#000"
+      buttonBgColor="#111"
+      buttonTextColor="#fff"
+      ease="power3.out"
+    />
+  );
+};
+
--- client/src/components/HomeScreen.jsx
+++ client/src/components/HomeScreen.jsx
@@ -1,15 +1,28 @@
 import { useState } from 'react'
 
 // Import components
+import DotGrid from './DotGrid.jsx'
 import { SearchBar } from './SearchBar';
 import { Header } from './Header';
 
 export default function HomeScreen() {
     return (
         <> 
+            <div style={{ width: '100%', height: '100vh', position: 'relative' }}>
+            <DotGrid 
+                dotSize={4}
+                gap={15}  
+                baseColor="#192225" 
+                activeColor="#124569"  
+                proximity={150} 
+                shockRadius={89}  
+                shockStrength={5}  
+                resistance={750}  
+                returnDuration={1.5}
+            />
+
             <Header />
-            <h1>Bruinplan</h1>
-            <SearchBar></SearchBar>
+            </div>
         </>
     );
 }
