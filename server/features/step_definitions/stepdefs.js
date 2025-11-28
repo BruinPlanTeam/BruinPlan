3770f20e1811593b8990a26fa531d06d926013a8
conorlazarparman <conorp@ucla.edu>
2025-11-27 16:44:28 -0800

    changed order of tests in file to match order they're run in on terminal. Sorry to viewers of past commits, the test numbers are out of order!

--- server/features/step_definitions/stepdefs.js
+++ server/features/step_definitions/stepdefs.js
@@ -17,7 +17,7 @@ async function teardown(){
     }
 }
 
-//Scenario: you should be able to create a new user
+//Scenario 1: you should be able to create a new user
 
 Given('there is no user yet with an email alice@example.com', async function () {
     teardown();
@@ -46,7 +46,50 @@ Then('I should be on the homepage with the new account signed in', function () {
            return 'pending';
          });
 
-//Scenario - You can't create a user that already exists
+//Scenario 2: You should be able to log in to an existing user account
+
+Given('there is a user with an email alice@example.com', async function () {
+               const newUser = {
+        email: 'alice@example.com',
+        username: 'testuser',
+        password: 'password123',
+    };
+
+    this.response = await request(app)
+        .post('/users')
+        .send(newUser);
+    });
+When('I try to log in with the email alice@example.com', async function () {
+            this.response = await request(app)
+            .get('/users')
+         });
+Then('I should be logged in', function () {
+           assert.strictEqual()
+         });
+Then('I should be on the homepage with the new account signed in', function () {
+           //Figure out how to test browser
+         });
+
+//Scenario 3: You should be not able to log in to a nonexistent user account
+
+Given('there is no user with an email alice@example.com', function () {
+        teardown()
+         });
+When('I try to log in with the email alice@example.com', async function () {
+        this.response = await request(app)
+            .get('/users')
+         });
+Then('I should not be logged in', function () {
+           const res = this.response 
+           //return 404 not found, rather than  
+           assert.strictEqual(res.status, 401, 'should return 401 for trying to operate without credentials.')
+         });
+Then('I should stay on the login page', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+//Scenario 4: You can't create a user that already exists
 Given('a user already exists with an email alice@example.com', async function () {
     const newUser = {
         email: 'alice@example.com',
@@ -81,47 +124,5 @@ Then('I should see an error message showing that the email is already in use', f
     assert.strictEqual(res.status, 409);
     });
 Then('I should remain on the sign-up page', function () {
-           return 'pending';
-         });
-
-
-//Scenario: You should be able to log in to an existing user account
-Given('there is a user with an email alice@example.com', async function () {
-               const newUser = {
-        email: 'alice@example.com',
-        username: 'testuser',
-        password: 'password123',
-    };
-
-    this.response = await request(app)
-        .post('/users')
-        .send(newUser);
-    });
-When('I try to log in with the email alice@example.com', async function () {
-            this.response = await request(app)
-            .get('/users')
-         });
-Then('I should be logged in', function () {
-           assert.strictEqual()
-         });
-Then('I should be on the homepage with the new account signed in', function () {
-           //Figure out how to test browser
-         });
-
-//Scenario: You should be not able to log in to a nonexistent user account
-Given('there is no user with an email alice@example.com', function () {
-        teardown()
-         });
-When('I try to log in with the email alice@example.com', async function () {
-        this.response = await request(app)
-            .get('/users')
-         });
-Then('I should not be logged in', function () {
-           const res = this.response 
-           //return 404 not found, rather than  
-           assert.strictEqual(res.status, 401, 'should return 401 for trying to operate without credentials.')
-         });
-Then('I should stay on the login page', function () {
-           // Write code here that turns the phrase above into concrete actions
            return 'pending';
          });
